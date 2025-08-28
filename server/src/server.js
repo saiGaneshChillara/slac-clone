8ad5bb4de56276281bee7c144b76e652ec9bfe63
saiGaneshChillara <saiganeshchillara@gmail.com>
2025-08-28 23:40:12 +0530

    auth iplemented

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,36 @@
 import express from 'express';
-import { ENV } from './config/env.js';
-import { connectDB } from './config/db.js';
 
+import { connectDB } from './config/db.js';
+import { ENV } from './config/env.js';
+
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express';
+import { functions, inngest } from './config/inngest.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/", (req, res) => {
   res.send("Hello world this");
 });
-app.listen(ENV.PORT, () => {
-  connectDB();
-  console.log(`Server started on ${ENV.PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV != 'production') {
+      app.listen(ENV.PORT, () => {
+        console.log(`Server started on ${ENV.PORT}`);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
