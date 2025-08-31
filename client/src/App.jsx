343ef2cc5789662472b0a034e581b0fa0369d84f
saiGaneshChillara <saiganeshchillara@gmail.com>
2025-08-31 20:32:52 +0530

    auth page implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 
 const App = () => {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
+    <>
       <SignedIn>
-        <UserButton />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </Routes>
       </SignedIn>
-    </header>
+      <SignedOut>
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="*" element={<Navigate to={"/auth"} replace />} />
+        </Routes>
+      </SignedOut>
+    </>
   );
 };
 
